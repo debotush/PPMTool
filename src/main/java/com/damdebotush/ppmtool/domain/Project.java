6e709070608cea059bee70ec631afd5d26fd28f4
Debotush Dam <damdebotush@gmail.com>
2022-03-28 21:12:00 +0600

    Project Repo

--- src/main/java/com/damdebotush/ppmtool/domain/Project.java
+++ src/main/java/com/damdebotush/ppmtool/domain/Project.java
@@ -1,4 +1,36 @@
 package com.damdebotush.ppmtool.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String projectName;
+
+    private String projectIdentifier;
+
+    private String description;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    protected Date created_at;
+
+    protected Date updated_At;
+
+    @PrePersist
+    public void setCreated_at() {
+        this.created_at = new Date();
+    }
+
+    @PreUpdate
+    public void setUpdated_At() {
+        this.updated_At = new Date();
+    }
 }
---
+++ src/main/java/com/damdebotush/ppmtool/repository/ProjectRepo.java
@@ -0,0 +1,15 @@
+package com.damdebotush.ppmtool.repository;
+
+import com.damdebotush.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProjectRepo extends CrudRepository<Project, Long> {
+
+    @Override
+    Optional<Project> findById(Long aLong);
+}
